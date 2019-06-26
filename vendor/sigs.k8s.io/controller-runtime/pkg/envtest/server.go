cec23b36cce0769d23a85fbaebeec3908f285e4a
r_takaishi <ryo.takaishi.0@gmail.com>
2019-06-27 08:28:43 +0900

    debug

--- vendor/sigs.k8s.io/controller-runtime/pkg/envtest/server.go
+++ vendor/sigs.k8s.io/controller-runtime/pkg/envtest/server.go
@@ -136,7 +136,7 @@ func (te *Environment) Start() (*rest.Config, error) {
 	} else {
 		te.ControlPlane = integration.ControlPlane{}
 		te.ControlPlane.APIServer = &integration.APIServer{Args: te.getAPIServerFlags()}
-		te.ControlPlane.Etcd = &integration.Etcd{Out: os.Stdout, Err: os.Stderr}
+		te.ControlPlane.Etcd = &integration.Etcd{Out: os.Stdout, Err: os.Stdout}
 
 		if os.Getenv(envKubeAPIServerBin) == "" {
 			te.ControlPlane.APIServer.Path = defaultAssetPath("kube-apiserver")
